0df7bd66259249c74813389fa0c0d35db9788b15
raisack8 <raisack8@gmail.com>
2022-09-18 23:14:37 +0900

    複数サイトを作成してみた。処理実装はまだまだ。

--- app.py
+++ app.py
@@ -8,9 +8,27 @@ bootstrap=Bootstrap(app)
 def index():
     return render_template('index.html')
 
+#----- Flask Practice -----
+
 @app.route('/practice/top',methods=['GET','POST'])
 def p_top():
     return render_template('practice/top.html')
+
+@app.route('/practice/pre_color',methods=['GET','POST'])
+def pre_color():
+    return render_template('practice/pre_color.html')
+
+@app.route('/practice/pre_food',methods=['GET','POST'])
+def pre_food():
+    return render_template('practice/pre_food.html')
+
+@app.route('/practice/pre_season',methods=['GET','POST'])
+def pre_season():
+    return render_template('practice/pre_season.html')
+
+@app.route('/practice/pre_result',methods=['GET','POST'])
+def pre_result():
+    return render_template('practice/pre_result.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
